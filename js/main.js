6009e0cfdb8279e65046f39f922afab78cafc000
Flávio Queiroz <vinhorozz@hotmail.com>
2025-08-10 14:28:11 -0300

    moch:teste de conexão como dados dos pacientes e médicos no console

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ async function loadData() {
     const responseDoc=await fetch("./data/doctors.json");
     const responsePatient=await fetch("./data/patients.json");
     
-    console.log(responseDoc);
-    console.log(responsePatient)
+    const DocsData= await responseDoc.json();
+    const patData=await responsePatient.json();
+
+    console.log(DocsData);
+    console.log(patData);
+    // console.log(responseDoc);
+    // console.log(responsePatient);
+
+
 }
+
+loadData();
